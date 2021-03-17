9b2ecd3c5a2d2b20dd8e94709ad6b037c697719b
Puneeth K <puneethk.2899@gmail.com>
2021-03-17 19:39:27 +0530

    Added phone and password validator

---
+++ components/basic/utils/Validator.js
@@ -0,0 +1,34 @@
+export default class Validator {
+  constructor() {}
+
+  validatePhone(phone) {
+    let validation = {
+      isValid: true,
+      errorMessage: '',
+    };
+    let regex = /^\d{10}$/;
+    if (phone.length < 1) {
+      validation.isValid = false;
+      validation.errorMessage = 'Please enter your phone number';
+    } else if (!regex.test(phone)) {
+      validation.isValid = false;
+      validation.errorMessage = 'Please enter a valid phone number';
+    }
+    return validation;
+  }
+
+  validatePassword(password) {
+    let validation = {
+      isValid: true,
+      errorMessage: '',
+    };
+    if (password.length < 1) {
+      validation.isValid = false;
+      validation.errorMessage = 'Please enter a password';
+    } else if (password.length < 6) {
+      validation.isValid = false;
+      validation.errorMessage = 'Minimum password length is 6';
+    }
+    return validation;
+  }
+}
