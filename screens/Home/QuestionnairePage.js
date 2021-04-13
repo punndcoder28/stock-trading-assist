03022367d725ac0963d3c6ca488317c5b0a09032
Puneeth K <puneethk.2899@gmail.com>
2021-04-13 11:23:46 +0530

    Added questionnaire template

--- screens/Home/QuestionnairePage.js
+++ screens/Home/QuestionnairePage.js
@@ -1,8 +1,83 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import Button from '../../components/basic/Button';
 
 export default class QuestionnairePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: 0,
+      answers: [],
+    };
+  }
+  submitAnswer(answer) {
+    if (this.state.question === 3) {
+      // submit to backend
+      let answers = this.state.answers;
+      answers.push(answer);
+      this.setState({
+        answers: answers,
+      });
+      console.log(this.state);
+    } else {
+      let answers = this.state.answers;
+      answers.push(answer);
+      this.setState({
+        question: this.state.question + 1,
+        answers: answers,
+      });
+    }
+  }
   render() {
-    return <View />;
+    return (
+      <View style={{flex: 1}}>
+        {this.state.question === 0 && (
+          <View style={styles.questionContainer}>
+            <Text>Question 1</Text>
+            <Text>Answer 1</Text>
+            <Button mode="contained" onPress={() => this.submitAnswer(1)}>
+              Next
+            </Button>
+          </View>
+        )}
+        {this.state.question === 1 && (
+          <View style={styles.questionContainer}>
+            <Text>Question 2</Text>
+            <Text>Answer 2</Text>
+            <Button mode="contained" onPress={() => this.submitAnswer(2)}>
+              Next
+            </Button>
+          </View>
+        )}
+        {this.state.question === 2 && (
+          <View style={styles.questionContainer}>
+            <Text>Question 3</Text>
+            <Text>Answer 3</Text>
+            <Button mode="contained" onPress={() => this.submitAnswer(3)}>
+              Next
+            </Button>
+          </View>
+        )}
+        {this.state.question === 3 && (
+          <View style={styles.questionContainer}>
+            <Text>Question 4</Text>
+            <Text>Answer 4</Text>
+            <Button mode="contained" onPress={() => this.submitAnswer(4)}>
+              Submit
+            </Button>
+          </View>
+        )}
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  questionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 10,
+  },
+});
