a3449ff45d0c39f1e3af033ac43b0c88a9d376ea
Puneeth K <puneethk.2899@gmail.com>
2021-04-17 11:40:45 +0530

    Show list of companies in home page

--- screens/Home/HomePage.js
+++ screens/Home/HomePage.js
@@ -1,8 +1,55 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, Platform} from 'react-native';
+
+const companies = [
+  {
+    name: 'Company 1',
+    cagr: '15%',
+  },
+  {
+    name: 'Company 2',
+    cagr: '15%',
+  },
+  {
+    name: 'Company 3',
+    cagr: '15%',
+  },
+  {
+    name: 'Company 4',
+    cagr: '15%',
+  },
+  {
+    name: 'Company 5',
+    cagr: '15%',
+  },
+];
 
 export default class HomePage extends React.Component {
   render() {
-    return <View />;
+    return (
+      <View style={{flex: 1, marginTop: Platform.OS === 'ios' ? 50 : 0}}>
+        <Text style={{alignSelf: 'center'}}>List of companies</Text>
+        <View style={{flex: 1}}>
+          {companies.map((company, index) => {
+            return <CompanyItem company={company} key={index} />;
+          })}
+        </View>
+      </View>
+    );
   }
 }
+
+const CompanyItem = props => {
+  return (
+    <View style={{padding: 10}}>
+      <View style={{flexDirection: 'row'}}>
+        <Text>Name: </Text>
+        <Text>{props.company.name}</Text>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Text>CAGR: </Text>
+        <Text>{props.company.cagr}</Text>
+      </View>
+    </View>
+  );
+};
